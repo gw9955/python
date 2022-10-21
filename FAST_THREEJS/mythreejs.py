78f088b1af7a9bdd223d17238aa567c84a0a827e
gw9955 <shem9920@gmail.com>
2022-10-21 18:03:17 +0900

    1021 commit

---
+++ FAST_THREEJS/mythreejs.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import pymysql
+from fastapi import FastAPI, Request, Form
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+@app.get("/threejs", response_class=HTMLResponse)
+async def threejs(request: Request):
+    return templates.TemplateResponse("threejs.html", {"request": request})
+
+
+
+
+# uvicorn mythreejs:app --reload
